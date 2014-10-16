de41171df853f43da1fb2ed09057d9b001e94b31
Keng Kiat Lim <kkin@crnrc.com>
2014-10-16 15:46:46 +0800

    Added argument parser code

--- game_engine.py
+++ game_engine.py
@@ -1,1 +1,10 @@
 import sys
+from argparse import ArgumentParser
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('agents', help='List of agents to simulate', nargs='+')
+    args = parser.parse_args()
+    agents = args.agents
+
+    print(agents)
