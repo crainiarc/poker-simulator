abdac8b5bc272c258706d945d1cdc56f3a30ee1e
Keng Kiat Lim <kkin@crnrc.com>
2014-10-16 16:09:36 +0800

    Implemented way to shuffle and represent the deck

--- game_engine.py
+++ game_engine.py
@@ -1,10 +1,24 @@
 import sys
 from argparse import ArgumentParser
+from random import shuffle
 
-if __name__ == '__main__':
+cards = 'D2D3D4D5D6D7D8D9DTDJDQDKDA'
+cards += 'C2C3C4C5C6C7C8C9CTCJCQCKCA'
+cards += 'H2H3H4H5H6H7H8H9HTHJHQHKHA'
+cards += 'S2S3S4S5S6S7S8S9STSJSQSKSA'
+
+def shuffle_deck():
+    """Returns a random list of indexes pointing to the positions in the cards deck"""
+    deck = [i for i in range(0, 52)]
+    shuffle(deck)
+    return [cards[c*2:c*2+2] for c in deck]
+
+def parse_args():
+    """Parses the arguments given from the command line"""
     parser = ArgumentParser()
     parser.add_argument('agents', help='List of agents to simulate', nargs='+')
-    args = parser.parse_args()
-    agents = args.agents
+    return parser.parse_args()
 
-    print(agents)
+if __name__ == '__main__':
+    args = parse_args()
+    agents = args.agents
