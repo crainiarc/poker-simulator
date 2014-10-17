296bc418d48924c7dc0eaf9a6ea4743672fcf160
Keng Kiat Lim <kkin@crnrc.com>
2014-10-18 03:45:06 +0800

    Refactored the game_engine into its own class

--- game_engine.py
+++ game_engine.py
@@ -11,64 +11,103 @@ cards += 'S2S3S4S5S6S7S8S9STSJSQSKSA'
 big_blind = 100
 small_blind = 50
 
-def shuffle_deck():
-    """Returns a random list of indexes pointing to the positions in the cards deck"""
-    deck = [i for i in range(0, 52)]
-    shuffle(deck)
-    return [cards[c*2:c*2+2] for c in deck]
+class GameEngine:
+    def __init__(self, agents, buyin):
+        self.agents = shuffle(agents);
+        self.chips = [buyin] * len(agents)
 
-def deal_cards(agent, param, bet_hist):
-    """Calls the deal methods for the agent, with the relevant parameters"""
-    return agent.deal(param, bet_hist)
+    def new_game(self):
+        self.in_game = [True] * len(self.agents)
+        self.in_game_count = len(self.agents)
+        self.bet_hist = []
+        self.pot = big_blind + small_blind
+        self.chips[-2] -= big_blind
+        self.chips[-1] -= small_blind
 
-def flop_round(agent, param, bet_hist):
-    """Calls the flop methods for the agent, with the relevant parameters"""
-    return agent.flop(param, bet_hist)
+        self.hands = None
+        self.community_cards = []
 
-def turn_round(agent, param, bet_hist):
-    """Calls the turn methods for the agent, with the relevant parameters"""
-    return agent.turn(param, bet_hist)
+        self.shuffle_deck()
 
-def river_round(agent, param, bet_hist):
-    """Calls the river methods for the agent, with the relevant parameters"""
-    return agent.river(param, bet_hist)
+        for i in range(0, len(agents)):
+            self.agents[i].new_game(len(agents), i)
 
-def betting_round(agent, chips, in_game, bet_history, method, params):
-    """Simulates the betting round"""
-    bet_history += [[]]
-    in_game_count = sum(1 if playing else 0 for playing in in_game)
+    def shuffle_deck(self):
+        """Generates a random list of cards deck"""
+        deck = [i for i in range(0, 52)]
+        shuffle(deck)
+        self.deck = [cards[c*2:c*2+2] for c in deck]
 
-    bet_history[-1] = [normalize_bet(chips[0], method(agents[0], params[0], bet_history[-1]), 0)]
-    check = True if bet_history[-1][0] == 0 else False
-    max_bet = max(0, bet_history[-1][0])
-    bets_collected = bet_history[-1][0]
+    def deal_cards(self, agent, param):
+        """Calls the deal methods for the agent, with the relevant parameters"""
+        return agent.deal(param, self.bet_hist, self.pot)
 
-    raised_player = 0
-    i = (raised_player + 1) % len(agents)
+    def flop_round(self, agent, param):
+        """Calls the flop methods for the agent, with the relevant parameters"""
+        return agent.flop(param, self.bet_hist, self.pot)
 
-    while i != raised_player and in_game_count > 1:
-        if in_game[i]:
-            bet = normalize_bet(chips[i], method(agents[i], params[i], bet_history), max_bet)
-            chips[i] -= bet
-            bets_collected += bet
-            bet_history[-1] += [bet]
+    def turn_round(self, agent, param):
+        """Calls the turn methods for the agent, with the relevant parameters"""
+        return agent.turn(param, self.bet_hist, self.pot)
 
-            if bet > max_bet:
-                check = False
-                raised_player = i
-                max_bet = bet
+    def river_round(self, agent, param):
+        """Calls the river methods for the agent, with the relevant parameters"""
+        return agent.river(param, self.bet_hist, self.pot)
 
-            if bet == 0 and not check:
-                in_game[i] = False
-                in_game_count -= 1
+    def normalize_bet(chips, bet, curr_bet):
+        """Normalize the bet and make sure that the bets are within limits"""
+        return bet if bet <= chips and bet >= curr_bet else 0
 
-        i = (i + 1) % len(agents)
+    def betting_round(self, method, params):
+        """Simulates the betting round"""
+        self.bet_history += [[]]
 
-    return bets_collected
+        bet = [normalize_bet(chips[0], method(self.agents[0], params[0]), 0)]
+        check = True if bet == 0 else False
+        max_bet = max(0, bet)
+        self.pot -= bet
+        self.bet_history[-1] += [bet]
 
-def normalize_bet(chips, bet, curr_bet):
-    """Normalize the bet and make sure that the bets are within limits"""
-    return bet if bet <= chips and bet >= curr_bet else 0
+        raised_player = 0
+        i = (raised_player + 1) % len(agents)
+
+        while i != raised_player and self.in_game_count > 1:
+            if self.in_game[i]:
+                bet = normalize_bet(chips[i], method(self.agents[i], params[i]), max_bet)
+                chips[i] -= bet
+                self.pot -= bet
+                self.bet_history[-1] += [bet]
+
+                if bet > max_bet:
+                    check = False
+                    raised_player = i
+                    max_bet = bet
+
+                if bet == 0 and not check:
+                    self.in_game[i] = False
+                    self.in_game_count -= 1
+
+            i = (i + 1) % len(self.agents)
+
+    def run_game(self):
+        self.hands = [(self.deck.pop(), self.deck.pop()) for i in range(0, len(self.agents))]
+        betting_round(self.deal_cards, self.hands)
+        self.community_cards = []
+
+        params = [(self.deck.pop(), self.deck.pop(), self.deck.pop())] * len(self.agents)
+        self.community_cards.extend(params[0])
+        betting_round(self.flop_round, params)
+
+        params = [self.deck.pop()] * len(self.agents)
+        self.community_cards += [params[0]]
+        betting_round(self.turn_round, params)
+
+        params = [self.deck.pop()] * len(self.agents)
+        self.community_cards += [params[0]]
+        betting_round(self.river_round, params)
+
+    def evaluate_hands(self):
+        pass
 
 def parse_args():
     """Parses the arguments given from the command line"""
@@ -83,35 +122,8 @@ if __name__ == '__main__':
     # Instantiate all the agent classes
     buy_in = int(args.buyin)
     agents = [getattr(import_module('agents.' + a), a.title())(buy_in) for a in args.agents]
-    chips = [buy_in] * len(agents)
 
-    # Set up the current game
-    in_game = [True] * len(agents)
-    bet_history = []
-    pot = big_blind + small_blind
-    chips[-2] -= big_blind
-    chips[-1] -= small_blind
-
-    shuffle(agents)
-    deck = shuffle_deck()
-    for i in range(0, len(agents)):
-        agents[i].new_game(len(agents), i)
-
-    hands = [(deck.pop(), deck.pop()) for i in range(0, len(agents))]
-    pot += betting_round(agents, chips, in_game, bet_history, deal_cards, hands)
-    community_cards = []
-
-    params = [(deck.pop(), deck.pop(), deck.pop())] * len(agents)
-    community_cards.extend(params[0])
-    pot += betting_round(agents, chips, in_game, bet_history, flop_round, params)
-
-    params = [deck.pop()] * len(agents)
-    community_cards += [params[0]]
-    pot += betting_round(agents, chips, in_game, bet_history, turn_round, params)
-
-    params = [deck.pop()] * len(agents)
-    community_cards += [params[0]]
-    pot += betting_round(agents, chips, in_game, bet_history, river_round, params)
-
-    # Evaluate
-    evaluate_results()
+    game = GameEngine(agents, buyin)
+    game.new_game()
+    game.run_game()
+    game.evaluate_hands()
