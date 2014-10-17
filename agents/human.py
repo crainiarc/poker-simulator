bd403eb556482128d8f4f97656364a286c4f0e76
Keng Kiat Lim <kkin@crnrc.com>
2014-10-17 22:26:27 +0800

    Added stubs for human AI

--- agents/human.py
+++ agents/human.py
@@ -1,3 +1,33 @@
 class Human:
-    def __init__(self):
-        self.pot = 20000
+    def __init__(self, chips):
+        self.chips = chips
+        self.hand = None
+        self.community_cards = []
+        self.flop_cards = None
+        self.turn_cards = None
+        self.river_cards = None
+
+    def new_game(self, num_players, position):
+        pass
+    
+    def deal(self, hand, bet_hist):
+        self.hand = hand
+        return int(input(str(self.hand) + ' Pre-flop: '))
+
+    def flop(self, flop_cards, bet_hist):
+        self.community_cards.extend(flop_cards)
+        self.flop_cards = flop_cards[:]
+
+        return int(input(str(self.hand) + ' Flop - ' + str(self.community_cards) + ' :'))
+
+    def turn(self, turn_cards, bet_hist):
+        self.community_cards.append(turn_cards)
+        self.turn_cards = turn_cards
+
+        return int(input(str(self.hand) + ' Turn - ' + str(self.community_cards) + ' :'))
+
+    def river(self, river_cards, bet_hist):
+        self.community_cards.append(river_cards)
+        self.river_cards = river_cards
+
+        return int(input(str(self.hand) + ' River - ' + str(self.community_cards) + ' :'))
