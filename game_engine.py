65b6b4cbb3a8baf0179b1faaffa030c8c28fe5cf
Keng Kiat Lim <kkin@crnrc.com>
2014-10-17 18:54:38 +0800

    Added main skeleton for game_engine.py

--- game_engine.py
+++ game_engine.py
@@ -14,6 +14,33 @@ def shuffle_deck():
     shuffle(deck)
     return [cards[c*2:c*2+2] for c in deck]
 
+def deal_cards(agent, param, bet_hist):
+    return agent.deal(param, bet_hist)
+
+def flop_round(agents, param, bet_hist):
+    return agent.flop(param, bet_hist)
+
+def turn_round(agents, param, bet_hist):
+    return agent.turn(param, bet_hist)
+
+def river_round(agents, param, bet_hist):
+    return agent.river(param, bet_hist)
+
+def betting_round(agents, chips, method, params):
+    pass
+
+def normalize_bet(chips, bet):
+    """Normalize the bet and make sure that the bets are within limits"""
+    return bet if bet <= chips and bet >= chips else 0
+
+
+def remove_folded_agents(agents, chips, bet_hist):
+    """Remove agents and their corresponding chips from the lists if their bets are 0"""
+    for i in range(0, len(best_hist)):
+        if bet_hist[i] == 0:
+            agent.pop(i)
+            chips.pop(i)
+
 def parse_args():
     """Parses the arguments given from the command line"""
     parser = ArgumentParser()
@@ -25,4 +52,30 @@ if __name__ == '__main__':
     args = parse_args()
 
     # Instantiate all the agent classes
-    agents = [getattr(import_module('agents.' + a), a.title())() for a in args.agents]
+    buy_in = int(args.buyin)
+    global_agents = [getattr(import_module('agents.' + a), a.title())(buy_in) for a in args.agents]
+    global_chips = [buy_in] * len(agents)
+
+    # Set up the current game
+    agents = global_agents[:]
+    chips = global_chips[:]
+    shuffle(agents)
+    deck = shuffle_deck()
+    pot = 0
+
+    for i in range(0, len(agents)):
+        agents[i] = new_game(len(agents), i)
+
+    params = [(deck.pop(), deck.pop()) for i in range(0, len(agents))]
+    agents, chips = betting_round(agents, chips, deal_cards, params)
+
+    params = [deck.pop(), deck.pop(), deck.pop()] * len(agents)
+    agents, chips = betting_round(agents, chips, flop_round, params)
+
+    params = [deck.pop()] * len(agents)
+    agents, chips = betting_round(agents, chips, turn_round, params)
+
+    params = [deck.pop()] * len(agents)
+    agents, chips = betting_round(agents, chips, river_round, params)
+
+    # Evaluate
