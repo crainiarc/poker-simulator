f4bab1fb5163dab446e6eae75fc4860d22e4a00e
Keng Kiat Lim <kkin@crnrc.com>
2014-10-18 03:46:50 +0800

    Updated human.py stub to include pot information for each of the turn methods

--- agents/human.py
+++ agents/human.py
@@ -10,24 +10,27 @@ class Human:
     def new_game(self, num_players, position):
         pass
     
-    def deal(self, hand, bet_hist):
+    def deal(self, hand, bet_hist, pot):
         self.hand = hand
         return int(input(str(self.hand) + ' Pre-flop: '))
 
-    def flop(self, flop_cards, bet_hist):
+    def flop(self, flop_cards, bet_hist, pot):
         self.community_cards.extend(flop_cards)
         self.flop_cards = flop_cards[:]
 
         return int(input(str(self.hand) + ' Flop - ' + str(self.community_cards) + ' :'))
 
-    def turn(self, turn_cards, bet_hist):
+    def turn(self, turn_cards, bet_hist, pot):
         self.community_cards.append(turn_cards)
         self.turn_cards = turn_cards
 
         return int(input(str(self.hand) + ' Turn - ' + str(self.community_cards) + ' :'))
 
-    def river(self, river_cards, bet_hist):
+    def river(self, river_cards, bet_hist, pot):
         self.community_cards.append(river_cards)
         self.river_cards = river_cards
 
         return int(input(str(self.hand) + ' River - ' + str(self.community_cards) + ' :'))
+
+    def end_game(self, results):
+        pass
