8949f5a91c583d7c6462e62aca2e9efb01c8e7e1
Davin Choo <cxjdavin@gmail.com>
2014-10-23 11:56:42 +0800

    Added dueces. Started integration with game_engine

--- agents/human.py
+++ agents/human.py
@@ -15,19 +15,22 @@ class Human:
         return int(input(str(self.hand) + ' Pre-flop: '))
 
     def flop(self, flop_cards, bet_hist, pot):
-        self.community_cards.extend(flop_cards)
+        if len(self.community_cards) != 3:
+            self.community_cards.extend(flop_cards)
         self.flop_cards = flop_cards[:]
 
         return int(input(str(self.hand) + ' Flop - ' + str(self.community_cards) + ' :'))
 
     def turn(self, turn_cards, bet_hist, pot):
-        self.community_cards.append(turn_cards)
+        if len(self.community_cards) != 4:
+            self.community_cards.append(turn_cards)
         self.turn_cards = turn_cards
 
         return int(input(str(self.hand) + ' Turn - ' + str(self.community_cards) + ' :'))
 
     def river(self, river_cards, bet_hist, pot):
-        self.community_cards.append(river_cards)
+        if len(self.community_cards) != 5:
+            self.community_cards.append(river_cards)
         self.river_cards = river_cards
 
         return int(input(str(self.hand) + ' River - ' + str(self.community_cards) + ' :'))
